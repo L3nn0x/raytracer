ab81147b191c26bf874733ec61b9d52512e1baea
L3nn0x <dragon.super@laposte.net>
2017-11-02 01:05:24 +0100

    2Added ray

---
+++ src/ray.rs
@@ -0,0 +1,24 @@
+use vec3::Vec3;
+
+
+#[derive(Debug)]
+pub struct Ray {
+    origin: Vec3,
+    direction: Vec3
+}
+
+impl Ray {
+    pub fn point_at_parameter(&self, t: f32) -> Vec3 {
+        self.origin + self.direction * t
+    }
+}
+
+#[test]
+fn ray_point_at_parameter() {
+    let r = Ray{
+        origin: Vec3{x: 0.0, y: 0.0, z: 0.0},
+        direction: Vec3{x: 1.0, y: 0.0, z: 0.0}
+    };
+    let p = r.point_at_parameter(2.0);
+    assert!(p == Vec3{x: 2.0, y: 0.0, z: 0.0});
+}
