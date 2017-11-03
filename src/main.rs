d94748eb68df87fdd64da7bd713ea27c57687b4a
L3nn0x <dragon.super@laposte.net>
2017-11-03 01:27:10 +0100

    Added diffuse mat start

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod sphere;
 mod hitable_list;
 mod camera;
 
-use vec3::Vec3;
+use vec3::{Vec3, dot};
 use ray::Ray;
 use hitable_list::HitableList;
 use sphere::Sphere;
@@ -14,10 +14,20 @@ use camera::Camera;
 
 extern crate rand;
 
+fn random_in_unit_sphere() -> Vec3 {
+    loop {
+        let p = 2.0 * Vec3::new(rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>()) - Vec3::new(1.0, 1.0, 1.0);
+        if dot(&p, &p) >= 1.0 {
+            return p;
+        }
+    }
+}
+
 fn color(ray: Ray, world: &Hitable) -> Vec3 {
     let mut rec : HitRecord = Default::default();
     if world.hit(&ray, 0.0, 10000.0, &mut rec) {
-        return 0.5 * (rec.normal + Vec3::new(1.0, 1.0, 1.0));
+        let target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(Ray::new(rec.p, target - rec.p), world);
     }
     let u = ray.direction();
     let t = 0.5 * (u.y + 1.0);
