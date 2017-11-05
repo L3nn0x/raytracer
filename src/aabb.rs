ea730987a74c782b33c01f4ff6f2e324eed159c6
L3nn0x <dragon.super@laposte.net>
2017-11-05 18:58:02 +0100

    Simplified code

--- src/aabb.rs
+++ src/aabb.rs
@@ -30,7 +30,7 @@ impl AABB {
             let mut t0 = (self.min[a] - ray.origin[a]) * inv_d;
             let mut t1 = (self.min[a] - ray.origin[a]) * inv_d;
             if inv_d < 0.0 {
-                std::mem::swap(&mut t0, &mut t1);
+                (t0, t1) = (t1, t0);
             }
             let tmin = if t0 > tmin as f64 {
                 t0
