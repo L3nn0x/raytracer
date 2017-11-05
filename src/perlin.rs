812a1eb6d8ba2fb2ca236e087120cc4c7ab17c5f
L3nn0x <dragon.super@laposte.net>
2017-11-05 18:43:35 +0100

    Update code

--- src/perlin.rs
+++ src/perlin.rs
@@ -16,10 +16,23 @@ impl Perlin {
         let u = p.x - p.x.floor();
         let v = p.y - p.y.floor();
         let w = p.z - p.z.floor();
-        let i = ((4.0 * p.x) as i32 & 255) as usize;
-        let j = ((4.0 * p.y) as i32 & 255) as usize;
-        let k = ((4.0 * p.z) as i32 & 255) as usize;
-        self.ranfloat[(self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]) as usize]
+        let i = p.x.floor() as usize;
+        let j = p.y.floor() as usize;
+        let k = p.z.floor() as usize;
+        let mut c = [[[0.0; 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.ranfloat[(self.perm_x[(i + di) & 255] ^
+                                               self.perm_y[(j + dj) & 255] ^
+                                               self.perm_z[(k + dk) & 255]) as usize];
+                }
+            }
+        }
+        /*self.ranfloat[(self.perm_x[(i) & 255] ^
+                       self.perm_y[(j) & 255] ^
+                       self.perm_z[(k) & 255]) as usize]*/
+        trilinear_interp(c, u, v, w)
     }
 
     pub fn new() -> Perlin {
@@ -40,12 +53,32 @@ fn perlin_generate() -> [f64; 256] {
     p
 }
 
+fn permute(p: &mut [u8; 256]) {
+    for i in (0..256).rev() {
+        let target = (rand::random::<f64>() * (i + 1) as f64) as usize;
+        p.swap(i, target);
+    }
+}
+
 fn perlin_generate_perm() -> [u8; 256] {
     let mut p = [0; 256];
     for i in 0..256 {
         p[i] = i as u8;
     }
-    let mut rng = thread_rng();
-    rng.shuffle(&mut p);
+    permute(&mut p);
     p
+}
+
+fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    let mut accum: f64 = 0.0;
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                accum += (i as f64 * u + (1.0 - i as f64) * (1.0 - u)) *
+                         (j as f64 * v + (1.0 - j as f64) * (1.0 - v)) *
+                         (k as f64 * w + (1.0 - w as f64) * (1.0 - w)) * c[i][j][k];
+            }
+        }
+    }
+    accum
 }
